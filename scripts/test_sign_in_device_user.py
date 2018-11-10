8438923d59a0f8f4b9d2a8ee4ac819a679ddc4f8
singer <https://github.com/singerZhai/haokaihui.git>
2018-11-10 18:35:15 +0800

    完善

--- scripts/test_sign_in_device_user.py
+++ scripts/test_sign_in_device_user.py
@@ -1,3 +1,4 @@
+import allure
 import requests
 
 from base.base_action import get_url, get_params, get_res
@@ -7,4 +8,12 @@ class TestSignInDeviceUser:
 
     url = get_url('data', 'sign_in_device_user', 'url')
     params = get_params('data', 'sign_in_device_user', 'params')
-    res = get_res('data', )
+    res = get_res('data', 'sign_in_device_user', 'res')
+
+    # 注册设备用户接口
+    @allure.step('注册设备用户接口')
+    def test_sign_in_device_user(self):
+        r = requests.post(self.url, self.params)
+        res = r.json()
+        assert res['status'] == self.res['status']
+        assert res['msg'] == self.res['msg']
